e56acd19eb56a39e3284d3b11482ff760bf3764d
rbrazell1 <brazellrussell@gmail.com>
2021-02-22 10:01:45 -0700

    Added image display and spinner

--- app/src/main/java/edu/cnm/deepdive/animals/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/animals/MainActivity.java
@@ -2,20 +2,51 @@ package edu.cnm.deepdive.animals;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
 import edu.cnm.deepdive.animals.model.Animal;
 import edu.cnm.deepdive.animals.service.WebServiceProxy;
 import java.io.IOException;
 import java.util.List;
+import java.util.Random;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+  private WebView contentView;
+
+  private Spinner animalSelector;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    contentView = findViewById(R.id.content_view);
+    animalSelector = findViewById(R.id.animal_selector);
+    setupWebView();
+  }
+
+  private void setupWebView() {
+    contentView.setWebViewClient(new WebViewClient() {
+      @Override
+      public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+        return false;
+      }
+    });
+    WebSettings settings = contentView.getSettings();
+    settings.setSupportZoom(true);
+    settings.setBuiltInZoomControls(true);
+    settings.setDisplayZoomControls(false);
+    settings.setUseWideViewPort(true);
+    settings.setLoadWithOverviewMode(true);
     new Retriever().start();
+
+
   }
 
   private class Retriever extends Thread {
@@ -27,7 +58,15 @@ public class MainActivity extends AppCompatActivity {
             .getAnimals(BuildConfig.API_KEY)
             .execute();
         if (response.isSuccessful()) {
-          Log.d(getClass().getName(), response.body().toString());
+          List<Animal> animals = response.body();
+          Random rng = new Random();
+          String url = animals.get(rng.nextInt(animals.size())).getImageUrl();
+          runOnUiThread(() -> {
+            contentView.loadUrl(url);
+            ArrayAdapter<Animal> adapter = new ArrayAdapter<>(MainActivity.this,
+                android.R.layout.simple_dropdown_item_1line, animals);
+            animalSelector.setAdapter(adapter);
+          });
         } else {
           Log.e(getClass().getName(), response.message());
         }
--- app/src/main/java/edu/cnm/deepdive/animals/model/Animal.java
+++ app/src/main/java/edu/cnm/deepdive/animals/model/Animal.java
@@ -1,5 +1,6 @@
 package edu.cnm.deepdive.animals.model;
 
+import androidx.annotation.NonNull;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
@@ -27,5 +28,11 @@ public class Animal {
 
   public void setImageUrl(String imageUrl) {
     this.imageUrl = imageUrl;
+  }
+
+  @NonNull
+  @Override
+  public String toString() {
+    return name;
   }
 }
