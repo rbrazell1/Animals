eaacf4a01a238eec4f7974ff251c3e12367333f5
rbrazell1 <brazellrussell@gmail.com>
2021-02-22 11:31:16 -0700

    replaced thread with async task

--- app/src/main/java/edu/cnm/deepdive/animals/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/animals/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.cnm.deepdive.animals;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -35,8 +36,8 @@ public class MainActivity extends AppCompatActivity {
     animalSelector.setOnItemSelectedListener(new OnItemSelectedListener() {
       @Override
       public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-      Animal animal = (Animal) parent.getItemAtPosition(position);
-      contentView.loadUrl(animal.getImageUrl());
+        Animal animal = (Animal) parent.getItemAtPosition(position);
+        contentView.loadUrl(animal.getImageUrl());
       }
 
       @Override
@@ -59,37 +60,44 @@ public class MainActivity extends AppCompatActivity {
     settings.setDisplayZoomControls(false);
     settings.setUseWideViewPort(true);
     settings.setLoadWithOverviewMode(true);
-    new Retriever().start();
+    new RetrieverTask().execute();
 
 
   }
 
-  private class Retriever extends Thread {
+  private class RetrieverTask extends AsyncTask<Void, Void, List<Animal>> {
 
     @Override
-    public void run() {
+    protected void onPostExecute(List<Animal> animals) {
+      super.onPostExecute(animals);
+      String url = animals.get(0).getImageUrl();
+      adapter = new ArrayAdapter<>(
+          MainActivity.this, R.layout.item_animal_spinner, animals);
+      adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+      contentView.loadUrl(url);
+      animalSelector.setAdapter(adapter);
+
+    }
+
+    @Override
+    protected List<Animal> doInBackground(Void... voids) {
       try {
         Response<List<Animal>> response = WebServiceProxy.getInstance()
             .getAnimals(BuildConfig.API_KEY)
             .execute();
         if (response.isSuccessful()) {
-          List<Animal> animals = response.body();
-          String url = animals.get(0).getImageUrl();
-          adapter = new ArrayAdapter<>(
-              MainActivity.this, R.layout.item_animal_spinner, animals);
-          adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-          runOnUiThread(() -> {
-            contentView.loadUrl(url);
-            animalSelector.setAdapter(adapter);
-          });
+          return response.body();
         } else {
           Log.e(getClass().getName(), response.message());
+          cancel(true);
         }
       } catch (IOException e) {
         Log.e(getClass().getName(), e.getMessage(), e);
+        cancel(true);
       }
-
+      return null;
     }
+
   }
 
 }
